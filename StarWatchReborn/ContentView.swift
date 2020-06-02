895feaab3760d3adaef6eae6e88c61ba3f363050
Antonella <antonellacirma@gmail.com>
2020-06-02 12:48:01 +0200

    Initial Commit

---
+++ StarWatchReborn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StarWatchReborn
+//
+//  Created by Antonella Cirma on 02/06/2020.
+//  Copyright © 2020 Antonella Cirma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
